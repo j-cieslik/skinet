8d0ff70247cb1bed31b94db9f15fd03a3be5b375
Kuba <jciesmail@gmail.com>
2020-08-01 23:54:19 +0200

    Correct GetProduct metod

--- Core/Specifications/ProductsWithTypesAndBrandsSPecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSPecification.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -5,6 +7,13 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSPecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSPecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSPecification(int id)
+         : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
